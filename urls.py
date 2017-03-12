8c9d670dcc8767f39d3d15a839292aeaf7e718e2
Suhita Goswami <suhitag93@users.noreply.github.com>
2017-03-12 19:22:58 -0400

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url(r'^$', views.index, name='index'),
-    url(r'^pull_stream/',views.pull_stream, name = "pull_stream")
-	#url(r'^search/$', views.pull_stream),
- ]
