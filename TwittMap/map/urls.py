21a019959004759c20eab8640fd0a17adf930f28
Bhagya Lakshmi Gummalla <bhagyagummalla@172-16-21-152.DYNAPOOL.NYU.EDU>
2017-04-20 14:13:33 -0400

    New_Tweets change

--- TwittMap/map/urls.py
+++ TwittMap/map/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^pull_stream/',views.pull_stream, name = "pull_stream"),
-    url(r'^sns_test_handler/'. views.sns_test_handler, name = "sns_test_handler")
+    url(r'^sns_test_handler/', views.sns_test_handler, name = "sns_test_handler")
  ]
