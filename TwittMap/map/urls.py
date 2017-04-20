d135b77689bbc98aac4ade243071f6d30ec2d806
Bhagya Lakshmi Gummalla <bhagyagummalla@172-16-21-152.DYNAPOOL.NYU.EDU>
2017-04-20 19:42:11 -0400

    Change

--- TwittMap/map/urls.py
+++ TwittMap/map/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
     url(r'^pull_stream/',views.pull_stream, name = "pull_stream"),
     url(r'^sns_test_handler/', views.sns_test_handler, name = "sns_test_handler")
+    url(r'^poll_data/', views.poll_data, name = "poll_data")
  ]
